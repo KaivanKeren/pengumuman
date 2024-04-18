db6a27ab3f391204d60d93b293452598a444387d
KaivanKeren <127612039+KaivanKeren@users.noreply.github.com>
2024-04-18 07:22:10 +0700

    Update page.tsx

--- src/app/siswa/[nis]/page.tsx
+++ src/app/siswa/[nis]/page.tsx
@@ -69,7 +69,7 @@ export default function Siswa() {
   return (
     <div className="max-w-4xl mx-5 md:mx-auto bg-white rounded-md shadow-md p-4 sm:p-8 my-8">
       <div className="max-w-lg hidden mx-auto bg-white border rounded-md p-8 my-8">
-        <div id="siswa-info" className="flex flex-col p-16 -mt-6">
+        <div id="siswa-info" className="flex flex-col p-14 -mt-6">
           <div className="grid grid-cols-2 gap-4 mb-4">
             <div>
  <img
