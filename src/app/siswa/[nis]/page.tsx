6664b5e28ceae632dc125b0d6cc04246ff0a67ff
Ismail <127612039+KaivanKeren@users.noreply.github.com>
2024-05-03 16:02:54 +0700

    Update page.tsx

--- src/app/siswa/[nis]/page.tsx
+++ src/app/siswa/[nis]/page.tsx
@@ -169,8 +169,8 @@ const shortenName = (name : string) => {
 <div className="flex justify-end mb-4">
             <div className="text-right">
               <p className="text-sm mr-[121px]">Kudus, 6 Mei 2024</p>
-              <p className="text-sm mr-[138px]">Kepala Sekolah,</p>
-              <img className="w-7" src="/TTD.jpg" />
+              <p className="text-sm mr-[138px] mb-2">Kepala Sekolah,</p>
+              <img className="w-20" src="/TTD.jpg" />
               <p className="mr-3 md:mr-2 text-lg underline">BUDI SUSANTO, S.Pd, M.Pd</p>
               <p className="text-sm mr-[63px] md:mr-[65px]">NIP. 19670815 199512 1 003</p>
             </div>
