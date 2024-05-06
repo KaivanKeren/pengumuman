664f5523c9f094e48c53790e4a6c24752439174f
Ismail <127612039+KaivanKeren@users.noreply.github.com>
2024-05-06 09:56:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
 
       <footer className="bg-white rounded-lg shadow m-4">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://github.com/KaivanKeren/" class="hover:underline">Ismail</a>. TJKT
+          <span className="text-sm text-gray-500 sm:text-center">© 2024 <a href="https://github.com/KaivanKeren/" className="hover:underline text-blue-500">Ismail</a>. TJKT
           </span>
         </div>
       </footer>
